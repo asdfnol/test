0bf355df5f881edeaa7d502ab9e823956f4a6a68
nol <65435420+asdfnol@users.noreply.github.com>
2024-10-13 21:05:22 -0400

    new methods and shi

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         Methods m = new Methods();
-        System.out.println(m.factorial(5));
+        System.out.print("return an int greater than or equal to 0\nn: ");
+        int n = s.nextInt(); 
+        int[] a = m.fibSequence(n);
+        for ( int x : a ) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
     }
 }
--- src/main/Methods.java
+++ src/main/Methods.java
@@ -2,22 +2,26 @@ public class Methods {
     public int squared(int n) {
         return n * n;
     }
-    /**
-    public int fibonacci(int n, int n1) {
-        int sum = n + n1;
-        if (
-        // fibonacci(0, 1) returns fibonacci(1, 1)
-        // fibonacci(1, 1) returns fibonacci(1, 2)
-    }
-    **/
     public int factorial(int n) {
         if (n == 0) {
             return 1;
         }
         return n * factorial(n - 1);
-        // factorial(3) returns factorial(3 * factorial(2))
-        // factorial(2) returns factorial(2 * factorial(1))
-        // factorial(1) returns factorial(1 * factorial(0))
-        // factorial(0) returns 1
+    }
+
+    // returns nth nubmer in fibonacci sequence
+    public int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+    // returns first n numbers in fibonacci sequence
+    public int[] fibSequence(int n) {
+        int[] x = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            x[i] = fibonacci(i);
+        }
+        return x;
     }
 }
