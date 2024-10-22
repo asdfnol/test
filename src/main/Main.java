0b5554bac1908058393fd441349169a4bcef38c2
nol <65435420+asdfnol@users.noreply.github.com>
2024-10-21 21:28:53 -0400

    modular project structure and new methods

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,13 @@
+package main;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
         Methods m = new Methods();
-        System.out.print("return an int greater than or equal to 0\nn: ");
-        int n = s.nextInt(); 
-        int[] a = m.fibSequence(n);
-        for ( int x : a ) {
-            System.out.print(x + " ");
+        int x = 2147483647;
+        var b = m.intToBinary(x);
+        for ( int e : b ) {
+            System.out.print(e + "");
         }
         System.out.println();
     }
--- src/main/Methods.java
+++ src/main/Methods.java
@@ -1,14 +1,11 @@
+package main;
 public class Methods {
-    public int squared(int n) {
-        return n * n;
-    }
     public int factorial(int n) {
         if (n == 0) {
             return 1;
         }
         return n * factorial(n - 1);
     }
-
     // returns nth nubmer in fibonacci sequence
     public int fibonacci(int n) {
         if (n <= 1) {
@@ -23,5 +20,19 @@ public class Methods {
             x[i] = fibonacci(i);
         }
         return x;
+    }
+    public int[] intToBinary(int n) {
+        int[] t = new int[32];
+        for ( int i = 0; i < t.length; i++ ) {
+           t[i] = n % 2;
+           n = n / 2;
+        }
+        int[] b = new int[t.length];
+        int j = 0;
+        for ( int i = t.length - 1; i >= 0; i-- ) {
+           b[j] = t[i];
+           j++;
+        }
+        return b;
     }
 }
