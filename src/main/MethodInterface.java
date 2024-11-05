13103a40ee815d52082d7ec93fcac02c38062020
nol <65435420+asdfnol@users.noreply.github.com>
2024-11-05 16:00:47 -0500

    maybe they're cool

--- src/main/MethodInterface.java
+++ src/main/MethodInterface.java
@@ -2,6 +2,21 @@ package main;
 public interface MethodInterface {
     // interfaces might be more helpful in managing the methods
     // idk
+    /* remembering the language */
+    int[] intToBinary(int n);
     int fibonacci(int n);
     int[] fibSequence(int n);
+    double factorial(double n);
+    double choose(double n, double k);
+    int[][] transpose(int[][] a);
+    /* ALGORITHM PRACICE */
+    int[] arrayRotation(int[] a, int d);
+    int[] selectionSort(int[] initArr);
+    int[] insertionSort(int[] initArr);
+    /*
+    int[] mergeSort(int[] initArr);
+    int sequentialSearch(int[] arr, int x);
+    int binarySearch(int[] arr, int x);
+    int[] removeDuplicates(int[] initArr);
+    */
 }
